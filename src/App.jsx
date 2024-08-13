78b61da7ec5f2750b1e18583cfdb77698294329c
Gayatri2301 <gayathriedibilli@gmail.com>
2024-08-13 17:10:19 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ const App = () => {
   return (
     <div>
       {/* Navbar */}
-      <nav className="bg-cyan-100 p-4 shadow-md">
+      <nav className="bg-black p-4 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
           {/* Logo or Title */}
-          <div className="text-xl font-bold text-gray-800">
+          <div className="text-xl font-bold text-white">
             My Portfolio
           </div>
           
           {/* Navigation Links */}
           <div className="space-x-6">
-            <a href="#about" className="text-gray-800 hover:text-cyan-600">About Me</a>
-            <a href="#projects" className="text-gray-800 hover:text-cyan-600">Projects</a>
-            <a href="#contact" className="text-gray-800 hover:text-cyan-600">Contact</a>
+            <a href="#about" className="text-gray-800 text-white hover:text-cyan-600">About Me</a>
+            <a href="#projects" className="text-gray-800 text-white hover:text-cyan-600">Projects</a>
+            <a href="#contact" className="text-gray-800 text-white hover:text-cyan-600">Contact</a>
           </div>
         </div>
       </nav>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -22,10 +22,10 @@ import codechefIcon from '../assets/about/codechef.png';
 
 const Portfolio = () => {
   return (
-    <div className="bg-slate-100 min-h-screen p-6">
-      <header className="bg-cyan-500 p-4 rounded-md shadow-md mb-6">
-        <h1 className="text-4xl font-bold text-white text-center">EDIBILLI GAYATRI</h1>
-        <p className="text-center text-white mt-2">Full Stack Developer</p>
+    <div className="bg-slate-900 min-h-screen p-6 ">
+      <header className="bg-cyan-500 p-4 rounded-md shadow-md mb-6 bg-blue-900">
+        <h1 className="text-4xl font-bold text-white text-center">GAYATRI EDIBILLI</h1>
+        <p className="text-center text-white mt-2">Frontend Developer</p>
       </header>
 
       <section>
@@ -34,32 +34,41 @@ const Portfolio = () => {
         <a href="https://github.com/Gayatri2301/" target="_blank"><img src={githubIcon} alt="Python Icon" className="w-10 h-10 bg-gray-50 border-gray-400 mr-2" /></a>
         <a href="https://www.linkedin.com/in/gayathri-edibilli/" target="_blank"><img src={linkedIcon} alt="Python Icon" className="w-10 h-10 bg-gray-50 border-gray-400 mr-2" /></a>
         <a href="https://leetcode.com/u/Gayathri_Edibilli/" target="_blank"><img src={leetcodeIcon} alt="Python Icon" className="w-10 h-10 bg-gray-50 border-gray-400 mr-2" /></a>
-        <a href="https://www.codechef.com/users/nagasrinu289" target="_blank"><img src={codechefIcon} alt="Python Icon" className="w-10 h-10 bg-gray-50 border-gray-400 mr-2" /></a>
+        <a href="https://www.codechef.com/users/gayatriedibill" target="_blank"><img src={codechefIcon} alt="Python Icon" className="w-10 h-10 bg-gray-50 border-gray-400 mr-2" /></a>
 
 
       </div>
       </section>
 
-      <section className="mb-6">
-        <h2 className="text-2xl font-semibold text-slate-700 mb-4">Projects</h2>
-        
-        <div className="bg-white p-4 rounded-md shadow-md mb-4">
-          <h3 className="text-xl font-semibold">E-commerce Website</h3>
-          <p>Collaborated to develop a Product Shopping Website to provide an online platform for users to browse, search, and purchase products.</p>
+      <section className="mb-6 ">
+        <h2 className="text-2xl font-semibold text-slate-700 mb-4  text-white">Projects</h2>
+        <div className="bg-white p-4 rounded-md shadow-md mb-4 hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out">
+        <h3 className="text-xl font-semibold">E-commerce Website</h3>
+          <p>• Developed a responsive e-commerce web application using React.js for the frontend, Node.js and Express.js for the backend,
+and MongoDB for database management. Implementing Tailwind
+CSS for styling to ensure a sleek and modern interface.</p>
           <p>Technologies used: React.js, Node.js, Express.js, MongoDB</p>
+          <p>Implemented user authentication and authorization mechanisms
+to secure user data and access.
+</p>
           <a href="https://shopping-app-sooty-three.vercel.app/" className="text-cyan-600">Live URL</a> | <a href="https://github.com/Gayatri2301/shopping-app.git" className="text-cyan-600">GitHub Repo</a>
         </div>
 
-        <div className="bg-white p-4 rounded-md shadow-md">
-          <h3 className="text-xl font-semibold">Grocery List Website</h3>
-          <p>Developed a todo List application with user authentication to manage grocery items efficiently.</p>
+        <div className="bg-white p-4 rounded-md shadow-md mb-4 hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out">
+        <h3 className="text-xl font-semibold">ToDoList Application</h3>
+          <p>Developed a dynamic and responsive TodoList web application
+using React.js to enhance task management and productivity.
+</p>
           <p>Technologies used: React.js, Node.js, Express.js, MongoDB</p>
-          <a href="https://grocery-listapp.vercel.app/" className="text-cyan-600">Live URL</a> | <a href="https://github.com/nagasrinu289/groceryListapp" className="text-cyan-600">GitHub Repo</a>
+          <p> It allows users to efficiently manage their tasks with capabilities
+to add, delete, and update tasks, while ensuring data persistence
+and responsive design</p>
+          <a href="https://to-do-app-omega-dusky.vercel.app/" className="text-cyan-600">Live URL</a> | <a href="https://github.com/Gayatri2301/ToDoApp" className="text-cyan-600">GitHub Repo</a>
         </div>
       </section>
 
       <section className="mb-6">
-        <h2 className="text-2xl font-semibold text-slate-700 mb-4">Skills</h2>
+        <h2 className="text-2xl font-semibold text-slate-700 mb-4 text-white">Skills</h2>
         <div className="bg-white p-4 rounded-md shadow-md">
           <h3 className="text-xl font-semibold">Programming</h3>
           <div className="flex items-center">
@@ -79,7 +88,6 @@ const Portfolio = () => {
           <div className='flex mt-4 gap-4'>
           <img src={nodeIcon} alt="Python Icon" className="w-16 h-16 bg-gray-50 border-gray-400 mr-2" />
           <img src={expressIcon} alt="Python Icon" className="w-16 h-16 bg-gray-50 border-gray-400 mr-2" />
-          <img src={fastapiIcon} alt="Python Icon" className="w-16 h-16 bg-gray-50 border-gray-400 mr-2" />
 
           </div>
 
@@ -101,7 +109,7 @@ const Portfolio = () => {
       </section>
 
       <section className="mb-6">
-        <h2 className="text-2xl font-semibold text-slate-700 mb-4">Education</h2>
+        <h2 className="text-2xl font-semibold text-slate-700 mb-4 text-white">Education</h2>
         <div className="bg-white p-4 rounded-md shadow-md">
           <h3 className="text-xl font-semibold">B.Tech in Computer Science</h3>
           <p>Sasi Institute of Technology and Engineering (8.9 CGPA)</p>
@@ -109,7 +117,7 @@ const Portfolio = () => {
         </div>
         <div className="bg-white p-4 rounded-md shadow-md mt-4">
           <h3 className="text-xl font-semibold">Intermediate in MPC</h3>
-          <p>Aditya Junior College (91%)</p>
+          <p>Aditya Junior College (90%)</p>
           <p>Jun 2019 - Mar 2021</p>
         </div>
         <div className="bg-white p-4 rounded-md shadow-md mt-4">
@@ -120,12 +128,12 @@ const Portfolio = () => {
       </section>
 
       <section>
-        <h2 className="text-2xl font-semibold text-slate-700 mb-4">Achievements</h2>
+        <h2 className="text-2xl font-semibold text-slate-700 mb-4 text-white">Achievements</h2>
         <div className="bg-white p-4 rounded-md shadow-md">
           <h3 className="text-xl font-semibold">Competitive Programming</h3>
-          <p>Solved 300+ problems on CodeChef, achieving a 2-star rating.</p>
-          <p>Solved 100+ problems on LeetCode, with a rating of 1500+.</p>
-          <a href="https://www.codechef.com/users/gayatriedibili" className="text-cyan-600">CodeChef Profile</a> | <a href="https://leetcode.com/u/Gayathri_Edibilli/" className="text-cyan-600">LeetCode Profile</a>
+          <p>Solved 500+ problems on CodeChef, achieving a 2-star rating.</p>
+          <p>Solved 100+ problems on LeetCode, with a rating of 1400+.</p>
+          <a href="https://www.codechef.com/users/gayatriedibill" className="text-cyan-600">CodeChef Profile</a> | <a href="https://leetcode.com/u/Gayathri_Edibilli/" className="text-cyan-600">LeetCode Profile</a>
         </div>
       </section>
     </div>
